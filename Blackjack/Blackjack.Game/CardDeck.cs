f2bf47e2e1771b53832afd56514fac70212b422a
approachcircle <osc-mcd@protonmail.com>
2024-11-14 20:27:42 +0000

    replace one with ace card

--- Blackjack/Blackjack.Game/CardDeck.cs
+++ Blackjack/Blackjack.Game/CardDeck.cs
@@ -8,7 +8,6 @@ public static class CardDeck
     public static Dictionary<string, int> CardValues =>
         new()
         {
-            { "One", 1 },
             { "Two", 2 },
             { "Three", 3 },
             { "Four", 4 },
@@ -32,9 +31,9 @@ public static class CardDeck
         {
             if (cardModelSymbol != null) return cardModelSymbol;
             var symbols = new Dictionary<string, string>();
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < CardValues.Count - 4; i++)
             {
-                symbols.Add(CardValues.Keys.ElementAt(i), (i + 1).ToString());
+                symbols.Add(CardValues.Keys.ElementAt(i), CardValues.Values.ElementAt(i).ToString());
             }
             symbols.Add("King", "K");
             symbols.Add("Queen", "Q");
