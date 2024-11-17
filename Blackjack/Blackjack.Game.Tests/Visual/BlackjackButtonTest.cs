bf94bfe269f8c6a9068e6554daf4e0a6b3f7cc4b
approachcircle <osc-mcd@protonmail.com>
2024-11-17 18:19:22 +0000

    add corresponding test for button

---
+++ Blackjack/Blackjack.Game.Tests/Visual/BlackjackButtonTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Sprites;
+
+namespace Blackjack.Game.Tests.Visual;
+
+[TestFixture]
+public sealed partial class BlackjackButtonTest : BlackjackTestScene
+{
+    private BlackjackButton button;
+    private SpriteText text;
+
+    public BlackjackButtonTest()
+    {
+        button = new BlackjackButton();
+        button.Text = "test button";
+        text = new SpriteText
+        {
+            Anchor = Anchor.Centre,
+            Origin = Anchor.Centre,
+            Y = 50,
+            Text = "test text",
+            Font = FontUsage.Default.With(size: 50),
+            Alpha = 0f
+        };
+        button.Action = () =>
+        {
+            text.Alpha = text.Alpha.Equals(0) ? 1 : 0;
+        };
+        Add(button);
+        Add(text);
+    }
+
+    [Test]
+    public void TestButtonBehaviour()
+    {
+        AddAssert("check button is present", () => button.IsPresent);
+        AddStep("make text is not present", () => text.Alpha = 0f);
+        AddStep("click button", () => button.TriggerClick());
+        AddAssert("check text is now present", () => text.IsPresent);
+    }
+}
