99984a3d7d1328d2ac46513bbdbbd8cee43f04f3
approachcircle <osc-mcd@protonmail.com>
2024-11-17 22:39:30 +0000

    refactor: magic numbers -> constants

--- Blackjack/Blackjack.Game/BlackjackScreen.cs
+++ Blackjack/Blackjack.Game/BlackjackScreen.cs
@@ -8,17 +8,19 @@ namespace Blackjack.Game;
 
 public partial class BlackjackScreen : Screen
 {
+    private const float shrink_size = 0.8f;
+    private const int animation_time = 200;
     public override void OnEntering(ScreenTransitionEvent e)
     {
-        Scale = new Vector2(0.8f, 0.8f);
-        this.ScaleTo(1f, 200, Easing.OutQuint);
-        this.FadeInFromZero(200, Easing.OutQuint);
+        Scale = new Vector2(shrink_size, shrink_size);
+        this.ScaleTo(1f, animation_time, Easing.OutQuint);
+        this.FadeInFromZero(animation_time, Easing.OutQuint);
     }
 
     public override bool OnExiting(ScreenExitEvent e)
     {
-        this.ScaleTo(0.8f, 200, Easing.InQuint);
-        this.FadeOut(200, Easing.InQuint);
+        this.ScaleTo(shrink_size, animation_time, Easing.InQuint);
+        this.FadeOut(animation_time, Easing.InQuint);
         return false;
     }
 
@@ -30,8 +32,11 @@ public partial class BlackjackScreen : Screen
     public override void OnResuming(ScreenTransitionEvent e)
     {
         Alpha = 0f;
-        Scale = new Vector2(0.8f, 0.8f);
-        this.Delay(200).ScaleTo(1f, 200, Easing.OutQuint).FadeInFromZero(200, Easing.OutQuint);
+        Scale = new Vector2(shrink_size, shrink_size);
+        this
+            .Delay(animation_time)
+            .ScaleTo(1f, animation_time, Easing.OutQuint)
+            .FadeInFromZero(animation_time, Easing.OutQuint);
     }
 
     protected override bool OnKeyDown(KeyDownEvent e)
@@ -44,7 +49,6 @@ public partial class BlackjackScreen : Screen
                 return true;
             }
         }
-
         return base.OnKeyDown(e);
     }
 }
