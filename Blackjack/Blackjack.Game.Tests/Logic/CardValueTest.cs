468561ea43e4d010f3f83435809a1ffb8bada49b
approachcircle <osc-mcd@protonmail.com>
2024-11-17 00:30:11 +0000

    implement ace logic, and add a corresponding test for it

---
+++ Blackjack/Blackjack.Game.Tests/Logic/CardValueTest.cs
@@ -0,0 +1,29 @@
+﻿using Blackjack.Game.Tests.Visual;
+using NUnit.Framework;
+using osu.Framework.Screens;
+
+namespace Blackjack.Game.Tests.Logic;
+
+[TestFixture]
+public partial class CardValueTest : BlackjackTestScene
+{
+    private MainScreen mainScreen;
+
+    public CardValueTest()
+    {
+        mainScreen = new MainScreen();
+        Add(new ScreenStack(mainScreen));
+    }
+
+    [Test]
+    public void TestLowAceDoesNotCauseBust()
+    {
+        AddUntilStep("wait for screen to load", () => mainScreen.IsLoaded);
+        AddAssert("ensure player is not bust", () => mainScreen.BindableScore.Value < 21);
+        AddStep("add ace to player hand", () =>
+        {
+            mainScreen.OnCardDrawRequest(HandOwner.Player, card: "Ace");
+        });
+        AddAssert("ensure player is not bust", () => mainScreen.BindableScore.Value < 21);
+    }
+}
