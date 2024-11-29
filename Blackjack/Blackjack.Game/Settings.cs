95ee7883c62e78e978d6db7e1a9c52cba5dfe523
approachcircle <osc-mcd@protonmail.com>
2024-11-29 00:03:43 +0000

    temporarily disable five card charlie until fixed

--- Blackjack/Blackjack.Game/Settings.cs
+++ Blackjack/Blackjack.Game/Settings.cs
@@ -5,6 +5,6 @@ namespace Blackjack.Game;
 public static class Settings
 {
     public static Bindable<bool> SFXEnabled { get; } = new(true);
-    public static Bindable<bool> FiveCardCharlie { get; } = new(true);
+    public static Bindable<bool> FiveCardCharlie { get; } = new(false); // temporarily disabled by default
     public static Bindable<bool> FlashingButtons { get; } = new(true);
 }
